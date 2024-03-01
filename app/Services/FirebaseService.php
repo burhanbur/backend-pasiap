b16d9d0a238c1db00786b1bfb9312376c643d4da
Burhan Mafazi <burhanburdev@gmail.com>
2024-03-01 09:30:45 +0700

    add firebase response

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Services;
 
@@ -29,74 +29,88 @@ use ErrorException;
 
 class FirebaseService
 {
+	public $url;
+
+	function __construct()
+	{
+		$this->url = 'https://fcm.googleapis.com/fcm/send';
+	}
+
 	public function sendNotificationToOfficer($code = null, $catId = null)
 	{
 		$returnValue = [];
 		$tokens = [];
 
 		try {
+			$user = auth()->user();
+
 			$category = Category::find($catId);
 
-			$url = 'https://fcm.googleapis.com/fcm/send';
-	        $sql = "
+			$url = $this->url;
+			$sql = "
 	            select ft.token from fcm_tokens as ft 
 	            join users as usr on usr.id = ft.user_id 
 	            join model_has_roles as mhr on mhr.model_id = usr.id 
 	            where mhr.role_id = ?
 	        ";
 
-	        $query = DB::select($sql, [5]); // petugas
+			$query = DB::select($sql, [5]); // petugas
 
-	        foreach ($query as $key => $value) {
-	        	$tokens[] = $value->token;
-	        }
+			foreach ($query as $key => $value) {
+				$tokens[] = $value->token;
+			}
 
-	        $serverKey = env('FIREBASE_KEY');
-	  
-	        $data = [
-	            "registration_ids" => $tokens,
-	            "notification" => [
-	                "title" => 'Pemberitahuan ' . @$category->name,
-	                "body" => 'Ada laporan ' . @$category->name . ' di dekat lokasi Anda dengan kode ' . $code . '. Segera lakukan tindakan pencegahan!',
-	            ]
-	        ];
+			$serverKey = env('FIREBASE_KEY');
 
-	        $encodedData = json_encode($data);
-	    
-	        $headers = [
-	            'Authorization:key=' . $serverKey,
-	            'Content-Type: application/json',
-	        ];
-	    
-	        $ch = curl_init();
-	      
-	        curl_setopt($ch, CURLOPT_URL, $url);
-	        curl_setopt($ch, CURLOPT_POST, true);
-	        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-	        // Disabling SSL Certificate support temporarly
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+			$data = [
+				"registration_ids" => $tokens,
+				"notification" => [
+					"android" => [
+						'user_id' => @$user->id,
+						'role_id' => @$user->roles()->first()->id,
+						'role_name' => @$user->roles()->first()->name,
+					],
+					"title" => 'Pemberitahuan ' . @$category->name,
+					"body" => 'Ada laporan ' . @$category->name . ' di dekat lokasi Anda dengan kode ' . $code . '. Segera lakukan tindakan pencegahan!',
+				]
+			];
 
-	        $result = curl_exec($ch);
+			$encodedData = json_encode($data);
 
-	        if ($result === FALSE) {
-	            throw new Exception(curl_error($ch), 1);                
-	        }
+			$headers = [
+				'Authorization:key=' . $serverKey,
+				'Content-Type: application/json',
+			];
 
-	        curl_close($ch);
+			$ch = curl_init();
 
-	        $returnValue = [
-	        	'success' => true,
-	        	'message' => 'success'
-	        ];			
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+			// Disabling SSL Certificate support temporarly
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+			$result = curl_exec($ch);
+
+			if ($result === FALSE) {
+				throw new Exception(curl_error($ch), 1);
+			}
+
+			curl_close($ch);
+
+			$returnValue = [
+				'success' => true,
+				'message' => 'success'
+			];
 		} catch (Exception $ex) {
-	        $returnValue = [
-	        	'success' => false,
-	        	'message' => $ex->getMessage()
-	        ];
+			$returnValue = [
+				'success' => false,
+				'message' => $ex->getMessage()
+			];
 		}
 
 		// save to log notification
@@ -116,82 +130,89 @@ class FirebaseService
 		$returnValue = [];
 
 		try {
-			$url = 'https://fcm.googleapis.com/fcm/send';
-	        $sql = "
+			$user = auth()->user();
+
+			$url = $this->url;
+			$sql = "
 	            select ft.token from fcm_tokens as ft 
 	            join users as usr on usr.id = ft.user_id 
 	            where usr.id = ?
 	        ";
 
-	        $query = DB::select($sql, [$reported_by]);
+			$query = DB::select($sql, [$reported_by]);
 
-	        foreach ($query as $key => $value) {
-	        	$tokens[] = $value->token;
-	        }
+			foreach ($query as $key => $value) {
+				$tokens[] = $value->token;
+			}
 
-	        $serverKey = env('FIREBASE_KEY');
+			$serverKey = env('FIREBASE_KEY');
 
-	        switch ($status) {
-	        	case '1':
-	        		$message = 'Laporan Anda berhasil diproses, mohon tetap tenang.';
-	        		break;
+			switch ($status) {
+				case '1':
+					$message = 'Laporan Anda berhasil diproses, mohon tetap tenang.';
+					break;
 
-	        	case '2':
-	        		$message = 'Laporan Anda sedang ditangani oleh petugas, mohon tetap tenang dan mengikuti petunjuk petugas.';
-	        		break;
+				case '2':
+					$message = 'Laporan Anda sedang ditangani oleh petugas, mohon tetap tenang dan mengikuti petunjuk petugas.';
+					break;
 
-	        	case '3':
-	        		$message = 'Laporan Anda telah selesai ditangani oleh petugas.';
-	        		break;
-	        	
-	        	default:
-	        		$message = 'Tim telah tiba di lokasi. Mohon tetap tenang dan mengikuti petunjuk petugas.';
-	        		break;
-	        }
-	  
-	        $data = [
-	            "registration_ids" => $tokens,
-	            "notification" => [
-	                "title" => 'Status Penanganan Laporan',
-	                "body" => $message,
-	            ]
-	        ];
+				case '3':
+					$message = 'Laporan Anda telah selesai ditangani oleh petugas.';
+					break;
 
-	        $encodedData = json_encode($data);
-	    
-	        $headers = [
-	            'Authorization:key=' . $serverKey,
-	            'Content-Type: application/json',
-	        ];
-	    
-	        $ch = curl_init();
-	      
-	        curl_setopt($ch, CURLOPT_URL, $url);
-	        curl_setopt($ch, CURLOPT_POST, true);
-	        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+				default:
+					$message = 'Tim telah tiba di lokasi. Mohon tetap tenang dan mengikuti petunjuk petugas.';
+					break;
+			}
 
-	        $result = curl_exec($ch);
+			$data = [
+				"registration_ids" => $tokens,
+				"notification" => [
+					"android" => [
+						'user_id' => @$user->id,
+						'role_id' => @$user->roles()->first()->id,
+						'role_name' => @$user->roles()->first()->name,
+					],
+					"title" => 'Status Penanganan Laporan',
+					"body" => $message,
+				]
+			];
 
-	        if ($result === FALSE) {
-	            throw new Exception(curl_error($ch), 1);                
-	        }
+			$encodedData = json_encode($data);
 
-	        curl_close($ch);
+			$headers = [
+				'Authorization:key=' . $serverKey,
+				'Content-Type: application/json',
+			];
 
-	        $returnValue = [
-	        	'success' => true,
-	        	'message' => 'success'
-	        ];
+			$ch = curl_init();
+
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+			$result = curl_exec($ch);
+
+			if ($result === FALSE) {
+				throw new Exception(curl_error($ch), 1);
+			}
+
+			curl_close($ch);
+
+			$returnValue = [
+				'success' => true,
+				'message' => 'success'
+			];
 		} catch (Exception $ex) {
-	        $returnValue = [
-	        	'success' => false,
-	        	'message' => $ex->getMessage()
-	        ];
+			$returnValue = [
+				'success' => false,
+				'message' => $ex->getMessage()
+			];
 		}
 
 		// save to log notification
